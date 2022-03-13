6ab3e5bde25f9381b7b010ced721bad4f98eaaaf
xFissa <xfissa.s@gmail.com>
2022-03-14 00:11:05 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
+getRandomInt(1, 5);
 
 // взяла на сайте:https://www.cyberforum.ru/javascript/thread1165903.html
 function checkStringLength(stringToCheck, maxLength) {
@@ -12,3 +13,4 @@ function checkStringLength(stringToCheck, maxLength) {
   }
   return false;
 }
+checkStringLength('book', 5);
