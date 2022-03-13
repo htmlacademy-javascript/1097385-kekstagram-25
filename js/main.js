cae038848859fb25c93095601af89ad521057ab9
xFissa <xfissa.s@gmail.com>
2022-03-13 23:51:25 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // взяла данную функцию с этого сайта:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
 // взяла на сайте:https://www.cyberforum.ru/javascript/thread1165903.html
